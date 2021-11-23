570c3411689e681c71dbfe0b107e5c15c1cbaf88
Adan Nugraha <contact.ardana@gmail.com>
2021-11-24 04:37:44 +0700

    v1 finished

--- app.py
+++ app.py
@@ -16,14 +16,18 @@ db = SQL("sqlite:///birthdays.db")
 def index():
     if request.method == "POST":
 
-        # TODO: Add the user's entry into the database
-
+        # Add the user's entry into the database
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+             
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
+        
         return redirect("/")
 
     else:
 
-        # TODO: Display the entries in the database on index.html
-
-        return render_template("index.html")
-
-
+        # Display the entries in the database on index.html
+        birthdays = db.execute("SELECT * FROM birthdays")
+        
+        return render_template("index.html", birthdays=birthdays)
